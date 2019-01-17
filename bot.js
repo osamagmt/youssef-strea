bb24012d46a17c99f7ac4477fe8bd446b73b4851
osamagmt <34580348+osamagmt@users.noreply.github.com>
2019-01-17 15:16:45 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const adminprefix = "o";
-const developers = ["440081527909515265" , "474200581163057153"];
+const adminprefix = "y";
+const developers = ["525749212198207508" , "474200581163057153"];
 
 
 client.on('ready', () => {
